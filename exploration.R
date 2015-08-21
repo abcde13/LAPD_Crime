6dc2d862d5508e8adc986d4823776a3e73dc7e2e
Joraaver Chahal <jsc@Joraavers-MacBook-Pro.local>
2015-08-20 18:10:24 -0700

    gets density map of crime working

--- exploration.R
+++ exploration.R
@@ -1,4 +1,5 @@
 
+
 library(dplyr)
 library(stringr)
 library(ggmap)
@@ -6,9 +7,12 @@ library(shiny)
 
 LAPD_set <- 
   read.csv("~/Downloads/LAPD_Crime_and_Collision_Raw_Data_-_2014.csv",
-           na.strings="\"\"",colClasses=c(Location.1 = "double"),
+           na.strings="",
            stringsAsFactors=FALSE)
-
+na_locs = is.na(LAPD_set$Location.1)
+LAPD_set = LAPD_set[!na_locs,]
+traffic = LAPD_set$Crm.Cd.Desc == "TRAFFIC DR #"
+LAPD_Set = LAPD_set[!traffic,]
 loc = LAPD_set$Location.1
 loc = strsplit(LAPD_set$Location.1,c(','))
 lat_n = seq(1,length(unlist(loc)),2)
@@ -16,11 +20,23 @@ long_n = seq(2,length(unlist(loc)),2)
 lats = unlist(loc)[lat_n]
 longs = unlist(loc)[long_n]
 latlong = data.frame(lats,longs)
-latlong_n = sapply(head(latlong),function(x) {
+latlong = sapply(latlong,function(x) {
   x = gsub("[\\)]|[\\(]","",x,perl=T)
   x = str_trim(x)
-  print(x)
-  },simplify=T)
-lats = as.numeric(latlong_n[,1])
-longs = as.numeric(latlong_n[,2])
+},simplify=T)
+lats = as.numeric(latlong[,1])
+longs = as.numeric(latlong[,2])
 LAPD_set = mutate(LAPD_set,"Lat" = lats,"Long" = longs)
+
+geocode("Los Angeles")
+la_latlong = c(lon = -118.2437,lat = 34.05223)
+la_map = get_map(la_latlong,zoom=10,maptype="terrain")
+ggmap(la_map,extent="normal",maprange = F) %+% LAPD_set +
+  aes(x=Long,y=Lat) +
+  geom_density2d() +
+  stat_density2d(aes(fill = ..level.., alpha = ..level..),size=10,
+                 bins=16,geom="polygon") + 
+  scale_alpha(range=c(0.1,0.6)) +
+  coord_map(projection="mercator",
+            xlim=c(attr(la_map, "bb")$ll.lon, attr(la_map, "bb")$ur.lon),
+            ylim=c(attr(la_map, "bb")$ll.lat, attr(la_map, "bb")$ur.lat))
